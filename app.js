c1110648a24e40b5148a9c891b18783741e48657
zoe petropoulou <zoepetropoulou@zoes-MacBook-Air-3.local>
2023-02-13 11:16:08 -0500

    database copied

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e1976eb204mshed531a01eb0e2ddp195ff9jsn46d950e134a3',
+		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+	}
+};
+
+fetch('https://imdb8.p.rapidapi.com/title/find?q=game', options)
+	.then(response => response.json())
+	.then(response => console.log(response)
+	.catch(err => console.error(err));
