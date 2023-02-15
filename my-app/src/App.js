a89422f2293c9ba481bf0b3bc69c30861385404b
Galen Sho Sato <galen.sato@gmail.com>
2023-02-15 11:21:50 -0500

    Added Links

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,8 @@ import NavBar from "./Components/NavBar";
 import Form from "./Components/Form";
 import MovieDetails from "./Components/MovieDetails";
 
-import Search from "./Search";
+import Search from "./Components/Search";
+import TvDetails from "./Components/TvDetails";
 
 
 function App() {
@@ -28,8 +29,11 @@ function App() {
                 <Route path='/movies/:id'>
                     <MovieDetails />
                 </Route>
+                <Route path='/tvShow/:id'>
+                    <TvDetails />
+                </Route>
                 <Route  exact path='/'>
-                    <Container movieInfo={movieInfo} />
+                    <Container movieInfo={movieInfo} movieSearch={movieSearch} />
                 </Route>
             </Switch>
         </div>
--- my-app/src/Components/NavBar.js
+++ my-app/src/Components/NavBar.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import Search from "./Search"
 
 function NavBar() {
     return (
@@ -7,6 +8,7 @@ function NavBar() {
                 {/* <Link>Movie</Link>
                 <Link>Tv Show</Link> */}
                 <Link to='/add'>Add Movie/Tv Show</Link>
+                
             </nav>
         
     )
