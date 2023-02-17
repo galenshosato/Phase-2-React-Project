78b6835979b445b9516ec9545c56eb6f3a051cc0
Galen Sho Sato <galen.sato@gmail.com>
2023-02-17 09:42:18 -0500

    laptop changes

--- my-app/src/Components/NavBar.js
+++ my-app/src/Components/NavBar.js
@@ -6,12 +6,16 @@ function NavBar({movieSearch, setSearch, type, newMovie, show}) {
     const renderType = (type === 'movie' ? <Link to={`/movies/${newMovie.id}`}>Movie</Link> : <Link to={`/tvShow/${show.id}`}>Tv Show</Link>)
 
     return (
+        <header>
             <nav>
                 <Link to='/'>Home</Link>
                 {type ? renderType : null}
                 <Link to='/add'>Add Movie/Tv Show</Link>
-                <Search movieSearch={movieSearch} setSearch={setSearch}/>
             </nav>
+            <div>
+                <Search movieSearch={movieSearch} setSearch={setSearch}/>
+            </div>
+        </header>
 
         
     )
